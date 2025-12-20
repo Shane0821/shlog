a43a3619aecce28ee894b9a28b052325214c2b80
Shane <143796207+Shane0821@users.noreply.github.com>
2025-12-20 16:57:19 +0800

    feat: support multi logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -2,29 +2,63 @@
 
 namespace shlog {
 
-Logger::~Logger() {
-    stop_ = true;
+MTLogger::MTLogger() { stop_.store(true); }
+
+MTLogger::~MTLogger() { stop(); }
+
+void MTLogger::stop() {
+    stop_.store(true);
     if (processThread_.joinable()) {
         processThread_.join();
     }
 }
 
-void Logger::init(LogLevel level, SinkPtr sink) {
+void MTLogger::init(LogLevel level, SinkPtr sink) {
     std::lock_guard<std::mutex> lock(mutex_);
 
-    if (processThread_.joinable()) {
-        processThread_.join();
-    }
+    stop();
 
-    setLogLevel(level);
-    setLogSink(std::move(sink));
+    LoggerBase::init(level, std::move(sink));
 
     stop_.store(false);
     // Start the log processing thread
     processThread_ = std::thread([this] { processLogTasks(); });
 }
 
-void Logger::processLogTasks() {
+void MTLogger::processLogTasks() {
+    while (true) {
+        if (stop_ && taskQueue_.empty()) {
+            break;
+        }
+        std::function<void()> task{nullptr};
+        if (!taskQueue_.empty()) {
+            taskQueue_.pop(task);
+            if (task) task();
+        }
+    }
+}
+
+STLogger::STLogger() { stop_ = true; }
+
+STLogger::~STLogger() { stop(); }
+
+void STLogger::stop() {
+    stop_ = true;
+    if (processThread_.joinable()) {
+        processThread_.join();
+    }
+}
+
+void STLogger::init(LogLevel level, SinkPtr sink) {
+    stop();
+
+    LoggerBase::init(level, std::move(sink));
+    stop_ = false;
+    // Start the log processing thread
+    processThread_ = std::thread([this] { processLogTasks(); });
+}
+
+void STLogger::processLogTasks() {
     while (true) {
         if (stop_ && taskQueue_.empty()) {
             break;
--- src/logger.h
+++ src/logger.h
@@ -13,21 +13,61 @@
 
 #include "log_sink.h"
 #include "mpmc_queue.hpp"
+#include "noncopyable.h"
 #include "singleton.hpp"
+#include "spsc_queue.hpp"
 
 namespace shlog {
 
-enum class LogLevel { TRACE, DEBUG, INFO, WARNING, ERROR, FATAL };
+enum class LogLevel { TRACE, DEBUG, INFO, WARNING, ERROR, FATAL, NONE };
 
-class Logger : public Singleton<Logger> {
-    friend class Singleton<Logger>;
+class LoggerBase : noncopyable {
+   public:
+    void init(LogLevel level = LogLevel::INFO,
+              SinkPtr sink = std::make_unique<ConsoleSink>()) {
+        setLogLevel(level);
+        setLogSink(std::move(sink));
+    }
+
+    void setLogLevel(LogLevel level) { level_ = level; }
+    void setLogSink(SinkPtr sink) { sink_ = std::move(sink); }
+
+   protected:
+    LoggerBase() = default;
+    ~LoggerBase() = default;
+
+    template <LogLevel level>
+    static constexpr const char* levelToString() {
+        if constexpr (level == LogLevel::TRACE) {
+            return "TRACE";
+        } else if constexpr (level == LogLevel::DEBUG) {
+            return "DEBUG";
+        } else if constexpr (level == LogLevel::INFO) {
+            return "INFO";
+        } else if constexpr (level == LogLevel::WARNING) {
+            return "WARNING";
+        } else if constexpr (level == LogLevel::ERROR) {
+            return "ERROR";
+        } else if constexpr (level == LogLevel::FATAL) {
+            return "FATAL";
+        } else {
+            static_assert(level != level, "unknown log level");
+            return "UNKNOWN";
+        }
+    }
+
+    SinkPtr sink_{nullptr};
+    LogLevel level_{LogLevel::NONE};
+};
+
+class MTLogger : public LoggerBase, public Singleton<MTLogger> {
+    friend class Singleton<MTLogger>;
 
    public:
-    ~Logger();
+    ~MTLogger();
 
     // Initialize logger with output file
-    void init(LogLevel level = LogLevel::INFO,
-              std::unique_ptr<LogSinkBase> = std::make_unique<ConsoleSink>());
+    void init(LogLevel level = LogLevel::INFO, SinkPtr = std::make_unique<ConsoleSink>());
 
     // add a log task to the queue
     template <LogLevel Level, typename... Args>
@@ -46,82 +86,98 @@ class Logger : public Singleton<Logger> {
             });
     }
 
-    void setLogLevel(LogLevel level) { level_ = level; }
-    LogLevel getLogLevel() const { return level_; }
-
-    void setLogSink(SinkPtr sink) { sink_ = std::move(sink); }
+    void stop();
 
    protected:
-    Logger() = default;
-
-    Logger(const Logger&) = delete;
-    Logger& operator=(const Logger&) = delete;
-
-   private:
-    template <LogLevel level>
-    constexpr const char* levelToString() {
-        if constexpr (level == LogLevel::TRACE) {
-            return "TRACE";
-        } else if constexpr (level == LogLevel::DEBUG) {
-            return "DEBUG";
-        } else if constexpr (level == LogLevel::INFO) {
-            return "INFO";
-        } else if constexpr (level == LogLevel::WARNING) {
-            return "WARNING";
-        } else if constexpr (level == LogLevel::ERROR) {
-            return "ERROR";
-        } else if constexpr (level == LogLevel::FATAL) {
-            return "FATAL";
-        } else {
-            static_assert(level != level, "Unknown log level");
-            return "UNKNOWN";
-        }
-    }
+    MTLogger();
 
     // Process log tasks
     void processLogTasks();
 
-    SinkPtr sink_;
     std::mutex mutex_;
     MPMCQueue<std::function<void()>> taskQueue_;
     std::thread processThread_;
     std::atomic<bool> stop_;
-    LogLevel level_;
 };
+
+class STLogger : public LoggerBase, public Singleton<STLogger> {
+    friend class Singleton<STLogger>;
+
+   public:
+    ~STLogger();
+
+    // Initialize logger with output file
+    void init(LogLevel level = LogLevel::INFO, SinkPtr = std::make_unique<ConsoleSink>());
+
+    // add a log task to the queue
+    template <LogLevel Level, typename... Args>
+    void log(int line, const std::string& format, Args&&... args) {
+        if (Level < level_) return;
+
+        if (stop_) return;
+
+        taskQueue_.emplace([... args = std::forward<Args>(args), line, format,
+                            this]() mutable {
+            auto logLine =
+                fmt::format("[{}][{}][{}:{}]: {}\n", time(NULL), levelToString<Level>(),
+                            __FILE__, line, fmt::format(format, std::move(args)...));
+            sink_->log(logLine);
+        });
+    }
+
+    void stop();
+
+   protected:
+    STLogger();
+
+    // Process log tasks
+    void processLogTasks();
+
+    SPSCQueue<std::function<void()>> taskQueue_;
+    std::thread processThread_;
+    bool stop_;
+};
+
+using DefaultLogger = STLogger;
 
 #endif
 }
 
-#define SHLOG_INIT(level, ...) shlog::Logger::GetInst().init(level, ##__VA_ARGS__)
-#define SHLOG_LOGGER_INIT(logger, level, ...) \
-    shlog::logger::GetInst().init(level, ##__VA_ARGS__)
+#define SHLOG_INIT(level, ...) shlog::DefaultLogger::GetInst().init(level, ##__VA_ARGS__)
+#define SHLOG_LOGGER_INIT(logger, level, ...) logger::GetInst().init(level, ##__VA_ARGS__)
 
-#define SHLOG_TRACE(format, ...) \
-    shlog::Logger::GetInst().log<shlog::LogLevel::TRACE>(__LINE__, format, ##__VA_ARGS__)
+#define SHLOG_TRACE(format, ...)                                                  \
+    shlog::DefaultLogger::GetInst().log<shlog::LogLevel::TRACE>(__LINE__, format, \
+                                                                ##__VA_ARGS__)
 #define SHLOG_LOGGER_TRACE(logger, format, ...) \
-    shlog::logger::GetInst().log<shlog::LogLevel::TRACE>(__LINE__, format, ##__VA_ARGS__)
+    logger::GetInst().log<shlog::LogLevel::TRACE>(__LINE__, format, ##__VA_ARGS__)
 
-#define SHLOG_DEBUG(format, ...) \
-    shlog::Logger::GetInst().log<shlog::LogLevel::DEBUG>(__LINE__, format, ##__VA_ARGS__)
+#define SHLOG_DEBUG(format, ...)                                                  \
+    shlog::DefaultLogger::GetInst().log<shlog::LogLevel::DEBUG>(__LINE__, format, \
+                                                                ##__VA_ARGS__)
 #define SHLOG_LOGGER_DEBUG(logger, format, ...) \
-    shlog::logger::GetInst().log<shlog::LogLevel::DEBUG>(__LINE__, format, ##__VA_ARGS__)
+    logger::GetInst().log<shlog::LogLevel::DEBUG>(__LINE__, format, ##__VA_ARGS__)
 
-#define SHLOG_INFO(format, ...) \
-    shlog::Logger::GetInst().log<shlog::LogLevel::INFO>(__LINE__, format, ##__VA_ARGS__)
-#define SHLOG_LOGGER_INFO(flogger, ormat, ...) \
-    shlog::logger::GetInst().log<shlog::LogLevel::INFO>(__LINE__, format, ##__VA_ARGS__)
+#define SHLOG_INFO(format, ...)                                                  \
+    shlog::DefaultLogger::GetInst().log<shlog::LogLevel::INFO>(__LINE__, format, \
+                                                               ##__VA_ARGS__)
+#define SHLOG_LOGGER_INFO(logger, format, ...) \
+    logger::GetInst().log<shlog::LogLevel::INFO>(__LINE__, format, ##__VA_ARGS__)
 
-#define SHLOG_WARN(format, ...) \
-    shlog::Logger::GetInst().log<shlog::LogLevel::WARN>(__LINE__, format, ##__VA_ARGS__)
-#define SHLOG_LOGGER_WARN(flogger, ormat, ...) \
-    shlog::logger::GetInst().log<shlog::LogLevel::WARN>(__LINE__, format, ##__VA_ARGS__)
+#define SHLOG_WARN(format, ...)                                                  \
+    shlog::DefaultLogger::GetInst().log<shlog::LogLevel::WARN>(__LINE__, format, \
+                                                               ##__VA_ARGS__)
+#define SHLOG_LOGGER_WARN(logger, format, ...) \
+    logger::GetInst().log<shlog::LogLevel::WARN>(__LINE__, format, ##__VA_ARGS__)
 
-#define SHLOG_ERROR(format, ...) \
-    shlog::Logger::GetInst().log<shlog::LogLevel::ERROR>(__LINE__, format, ##__VA_ARGS__)
+#define SHLOG_ERROR(format, ...)                                                  \
+    shlog::DefaultLogger::GetInst().log<shlog::LogLevel::ERROR>(__LINE__, format, \
+                                                                ##__VA_ARGS__)
 #define SHLOG_LOGGER_ERROR(logger, format, ...) \
-    shlog::logger::GetInst().log<shlog::LogLevel::ERROR>(__LINE__, format, ##__VA_ARGS__)
+    logger::GetInst().log<shlog::LogLevel::ERROR>(__LINE__, format, ##__VA_ARGS__)
 
-#define SHLOG_FATAL(format, ...) \
-    shlog::Logger::GetInst().log<shlog::LogLevel::FATAL>(__LINE__, format, ##__VA_ARGS__)
+#define SHLOG_FATAL(format, ...)                                                  \
+    shlog::DefaultLogger::GetInst().log<shlog::LogLevel::FATAL>(__LINE__, format, \
+                                                                ##__VA_ARGS__)
 #define SHLOG_LOGGER_FATAL(logger, format, ...) \
-    shlog::logger::GetInst().log<shlog::LogLevel::FATAL>(__LINE__, format, ##__VA_ARGS__)
+    logger::GetInst().log<shlog::LogLevel::FATAL>(__LINE__, format, ##__VA_ARGS__)
